c693139808d9b2fdef0f84a5cc6e47e941cb6abf
jbatke <badkey@gmx.net>
2021-06-30 22:06:40 +0200

    wip

--- @vectorgrid/display.m
+++ @vectorgrid/display.m
@@ -2,7 +2,7 @@ function display(st)
 % display content of a vectorgrid g.
 
 
-% Copyright (c) 2018-2020 Johann-Markus Batke (johann-markus.batke@hs-emden-leer.de)
+% Copyright (c) 2018-2021 Johann-Markus Batke (johann-markus.batke@hs-emden-leer.de)
 % 
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
